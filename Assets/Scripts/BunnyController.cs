8b48f0b0a13d2069b1ce47ad776e502645e46584
Arnaw <arnawk@gmail.com>
2017-04-20 10:30:55 +0530

    First Commit
    Moved local file to main repo. This just has a bunny standing on a grass
    patch, and able to jump on click for all devices.

---
+++ Assets/Scripts/BunnyController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BunnyController : MonoBehaviour {
+    private Rigidbody2D myRigidBody;
+    public float bunnyJumpForce = 50f;
+	// Use this for initialization
+	void Start () {
+
+        myRigidBody = GetComponent<Rigidbody2D>();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if ((Input.GetButtonDown("Jump")) || (Input.GetMouseButtonDown(0)))
+        {
+            myRigidBody.AddForce(transform.up* bunnyJumpForce);
+        }
+       
+    }
+}
